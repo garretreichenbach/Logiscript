52908df28cb1b5fc23dd783f475b5a074e452e53
TheDerpGamer <garret2kool@gmail.com>
2020-06-02 12:23:31 -0700

    Update TerminalGUI.java

--- src/net/dovtech/logiscript/gui/TerminalGUI.java
+++ src/net/dovtech/logiscript/gui/TerminalGUI.java
@@ -1,70 +1,11 @@
 package net.dovtech.logiscript.gui;
 
-import org.schema.schine.graphicsengine.forms.gui.newgui.GUIResizableGrabbableWindow;
+import org.schema.schine.graphicsengine.forms.gui.newgui.GUIMainWindow;
 import org.schema.schine.graphicsengine.forms.gui.newgui.GUIWindowInterface;
-import org.schema.schine.graphicsengine.forms.gui.newgui.config.WindowPaletteInterface;
 import org.schema.schine.input.InputState;
 
-public class TerminalGUI extends GUIResizableGrabbableWindow implements GUIWindowInterface {
+public class TerminalGUI extends GUIMainWindow implements GUIWindowInterface {
     public TerminalGUI(InputState inputState, int i, int i1, String s) {
         super(inputState, i, i1, s);
-    }
-
-    public WindowPaletteInterface getWindowPalette() {
-        return null;
-    }
-
-    protected int getMinWidth() {
-        return 0;
-    }
-
-    protected int getMinHeight() {
-        return 0;
-    }
-
-    public int getInnerCornerDistX() {
-        return 0;
-    }
-
-    public int getInnerCornerTopDistY() {
-        return 0;
-    }
-
-    public int getInnerCornerBottomDistY() {
-        return 0;
-    }
-
-    public int getInnerHeigth() {
-        return 0;
-    }
-
-    public int getInnerWidth() {
-        return 0;
-    }
-
-    public int getInnerOffsetX() {
-        return 0;
-    }
-
-    public int getInnerOffsetY() {
-        return 0;
-    }
-
-    public int getInset() {
-        return 0;
-    }
-
-    public int getTopDist() {
-        return 0;
-    }
-
-    @Override
-    public void cleanUp() {
-
-    }
-
-    @Override
-    public void draw() {
-
     }
 }
