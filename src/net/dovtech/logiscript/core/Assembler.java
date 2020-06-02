d773d7866790653b63e1b5f7493ce3639d8da23a
turtleStew <38766126+turtleStew@users.noreply.github.com>
2020-06-02 16:53:44 -0400

    Delete Assembler.java

--- src/net/dovtech/logiscript/core/Assembler.java
+++
@@ -1,9 +0,0 @@
-package net.dovtech.logiscript.core;
-
-public class Assembler {
-    public byte[] assemble(String input, int maxDataSize) {
-        byte data[] = new byte[maxDataSize];
-        
-        return data;
-    }
-}
