90bf83e74103d3d64b92541c031bf7619d2e3dee
TheDerpGamer <garret2kool@gmail.com>
2020-06-02 12:17:12 -0700

    Update TerminalGUI.java

--- src/net/dovtech/logiscript/gui/TerminalGUI.java
+++ src/net/dovtech/logiscript/gui/TerminalGUI.java
@@ -1,77 +1,66 @@
 package net.dovtech.logiscript.gui;
 
 import org.schema.schine.graphicsengine.forms.gui.newgui.GUIResizableGrabbableWindow;
+import org.schema.schine.graphicsengine.forms.gui.newgui.GUIWindowInterface;
 import org.schema.schine.graphicsengine.forms.gui.newgui.config.WindowPaletteInterface;
 import org.schema.schine.input.InputState;
 
-public class TerminalGUI extends GUIResizableGrabbableWindow {
-
-    private WindowPaletteInterface windowPaletteInterface;
-    private int minWidth;
-    private int minHeight;
-    private int innerCornerDistX;
-    private int innerCornerTopDistY;
-    private int innerCornerBottomDistY;
-    private int innerHeight;
-    private int innerWidth;
-    private int innerOffsetX;
-    private int innerOffsetY;
-    private int inset;
-    private int topDist;
-
+public class TerminalGUI extends GUIResizableGrabbableWindow implements GUIWindowInterface {
     public TerminalGUI(InputState inputState, int i, int i1, String s) {
         super(inputState, i, i1, s);
     }
 
     public WindowPaletteInterface getWindowPalette() {
-        return windowPaletteInterface;
+        return null;
     }
 
     protected int getMinWidth() {
-        return minWidth;
+        return 0;
     }
 
     protected int getMinHeight() {
-        return minHeight;
+        return 0;
     }
 
     public int getInnerCornerDistX() {
-        return innerCornerDistX;
+        return 0;
     }
 
     public int getInnerCornerTopDistY() {
-        return innerCornerTopDistY;
+        return 0;
     }
 
     public int getInnerCornerBottomDistY() {
-        return innerCornerBottomDistY;
+        return 0;
     }
 
     public int getInnerHeigth() {
-        return innerHeight;
+        return 0;
     }
 
     public int getInnerWidth() {
-        return innerWidth;
+        return 0;
     }
 
     public int getInnerOffsetX() {
-        return innerOffsetX;
+        return 0;
     }
 
     public int getInnerOffsetY() {
-        return innerOffsetY;
+        return 0;
     }
 
     public int getInset() {
-        return inset;
+        return 0;
     }
 
     public int getTopDist() {
-        return topDist;
+        return 0;
     }
 
+    @Override
     public void cleanUp() {
+
     }
 
     @Override
