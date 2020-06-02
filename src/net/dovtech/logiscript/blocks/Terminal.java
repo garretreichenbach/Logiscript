2d2c3262a56032f9765b454b84551952c57498ed
TheDerpGamer <garret2kool@gmail.com>
2020-06-02 16:46:51 -0700

    Update Terminal.java

--- src/net/dovtech/logiscript/blocks/Terminal.java
+++ src/net/dovtech/logiscript/blocks/Terminal.java
@@ -2,15 +2,11 @@ package net.dovtech.logiscript.blocks;
 
 import api.config.BlockConfig;
 import api.element.block.Blocks;
-import api.entity.Player;
-import net.dovtech.logiscript.gui.TerminalGUI;
 import org.schema.game.common.data.element.ElementInformation;
-import org.schema.schine.input.InputState;
 
 public class Terminal {
 
     private ElementInformation blockInfo;
-    private TerminalGUI terminalGUI;
 
     public Terminal() {
         blockInfo = BlockConfig.newElement("Terminal", new short[] { 3200 });
@@ -25,13 +21,5 @@ public class Terminal {
 
     public ElementInformation getBlockInfo() {
         return blockInfo;
-    }
-
-    public void onBlockActivate(Player player, InputState inputState) {
-        if(blockInfo.canActivate) {
-            terminalGUI = new TerminalGUI(inputState, 300, 300, "TERMINAL");
-            //Todo:Set the terminal's text to the last text inputted into it
-            terminalGUI.draw();
-        }
     }
 }
