91c1efd0eae97b95556e954ed23b7bfd77db87ee
TheDerpGamer <garret2kool@gmail.com>
2020-06-03 13:10:30 -0700

    Update Programmable.java

--- src/net/dovtech/logiscript/interp/Programmable.java
+++ src/net/dovtech/logiscript/interp/Programmable.java
@@ -1,12 +1,12 @@
 package net.dovtech.logiscript.interp;
 
 public class Programmable {
-    public String src;
-    public int[] dat;
-    public int[] mem;
-    public boolean assembled = false;
+    private String[] src;
+    private int[] dat;
+    private int[] mem;
+    private boolean assembled = false;
 
-    Programmable(int datSize, int memSize) { //The ISA only allows up to 256 bytes of data and memory capacity
+    public Programmable(int datSize, int memSize) { //The ISA only allows up to 256 bytes of data and memory capacity
         dat = new int[datSize > 0 && datSize < 257? datSize : 256];
         mem = new int[memSize > 0 && memSize < 257? memSize : 256];
     }
