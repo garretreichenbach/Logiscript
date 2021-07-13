523303fb796a6fabd711ec32d11b9a472d69e56a
TheDerpGamer <garret2kool@gmail.com>
2021-07-13 10:11:02 -0700

    more fixes cus intellij dumb

--- old/program/ScriptProgram.java
+++
@@ -1,7 +0,0 @@
-package dovtech.logiscript.program;
-
-public class ScriptProgram {
-
-    private String name;
-    private String program;
-}
--- old/scripts/ExternalConnection.java
+++
@@ -1,6 +0,0 @@
-package dovtech.logiscript.scripts;
-
-public class ExternalConnection {
-
-    private long connectionIndex;
-}
