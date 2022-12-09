690c0fbd80e6a841c05206a049c91fa2051151e1
lupoCani <lupoCani314@gmail.com>
2022-12-10 00:25:27 +0100

    Better vararg logic.

--- src/main/java/luamade/luawrap/WrapMethod.java
+++ src/main/java/luamade/luawrap/WrapMethod.java
@@ -32,36 +32,28 @@ public class WrapMethod extends VarArgFunction {
         for (final int argc : ms.keySet()) {
             final Method m = ms.get(argc);
             final Class<?>[] argst = m.getParameterTypes();
-            final boolean isVrad = argst[argc-1] == Varargs.class;
+            final boolean isVrad = argc > 0 && argst[argc-1] == Varargs.class;
             VarArgFunction f = new VarArgFunction() {
                 @Override
                 public Varargs invoke(Varargs vargs) {
-                    System.err.println("Calling method " + (isVrad ? "(varadic)" : ""));
-                    //If the function isn't varadic, the argument count must match exactly
-                    //(+1 because vargs contains the object itsels too)
-                    //If the function is varadic, the final Vararg can be nil.
-                    if (!isVrad ? argc+1 != vargs.narg() : argc > vargs.narg())
-                        throw new LuaError("Incorrect argument count.");
-
+                    if (!isVrad ? argc+1 != vargs.narg() : argc > vargs.narg()) //If the function isn't varadic, the argument count must match exactly
+                        throw new LuaError("Incorrect argument count."); //If the function is varadic, the final Vararg can be nil.
                     if (!clazz.isInstance(vargs.arg1())) throw new LuaError("Method must be called with self.");
                     if (!m.isAnnotationPresent(LuaMadeCallable.class)) throw new LuaError("Method not LuaMadeCallable.");
 
-
                     try {
-                        
                         Object[] argv = new Object[argc];
-                        //
-                        for (int i = 0; i < argc; ++i) {
-                            Class<?> argt = argst[i];
-                            if (i+1 == argc && argt == Varargs.class) //if we're at the last parameter and the method asks for a Vararg
-                                if (argc == vargs.narg()) {argv[i] = NIL; break;} //If we're out of arguments, give a nil
-                                else {argv[i] = vargs.subargs(i+2); break;} //Else pack remaining arguments in a Vararg.
+                        LuaValue o = vargs.arg1();
 
-                            argv[i] = WrapUtils.unwrap(vargs.arg(i+2), argt);
-                            if (!argt.isInstance(argv[i]))
-                                throw new LuaError(String.format("Got %s, expected %s.", argv[i].getClass(), argt));
+                        for (int i = 0; i < argc; ++i) {
+                            vargs = vargs.subargs(2);
+
+                            if (i+1 == argc && argst[i] == Varargs.class)
+                                argv[i] = vargs;
+                            else
+                                argv[i] = WrapUtils.unwrap(vargs.arg1(), argst[i]);
                         }
-                        Object out = m.invoke(vargs.arg1(), argv);
+                        Object out = m.invoke(o, argv);
                         return WrapUtils.wrap(out);
 
                     } catch (IllegalAccessException | IllegalArgumentException e) {
