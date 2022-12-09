7a4ad475c4518226d1850fead0bbe03607be5eeb
lupoCani <lupoCani314@gmail.com>
2022-12-10 00:26:37 +0100

    Improved Lua vector class.

--- src/main/java/luamade/lua/LuaVec3i.java
+++ src/main/java/luamade/lua/LuaVec3i.java
@@ -4,25 +4,23 @@ import luamade.luawrap.LuaMadeCallable;
 import luamade.luawrap.LuaMadeUserdata;
 import org.schema.common.util.linAlg.Vector3i;
 
-public class LuaVec3 extends LuaMadeUserdata {
+public class LuaVec3i extends LuaMadeUserdata {
     public int xx;
     public int yy;
     public int zz;
 
-    public LuaVec3(int x, int y, int z) {
+    public LuaVec3i(int x, int y, int z) {
         xx = x;
         yy = y;
         zz = z;
     }
 
-    public LuaVec3(Vector3i v) {
-        this.LuaVec3(v.x, v.y, v.z);
+    public LuaVec3i(Vector3i v) {
+        this(v.x, v.y, v.z);
     }
 
-    public LuaVec3(LuaVec3 vec) {
-        xx = vec.xx;
-        yy = vec.yy;
-        zz = vec.zz;
+    public LuaVec3i(LuaVec3i v) {
+        this(v.xx, v.yy, v.zz);
     }
 
     @LuaMadeCallable
@@ -38,20 +36,23 @@ public class LuaVec3 extends LuaMadeUserdata {
         return zz;
     }
     @LuaMadeCallable
-    public Integer incx(Integer x) {
-        return xx += x;
+    public LuaVec3i incx(Integer x) {
+        xx += x;
+        return this;
     }
     @LuaMadeCallable
-    public Integer incy(Integer y) {
-        return yy += y;
+    public LuaVec3i incy(Integer y) {
+        yy += y;
+        return this;
     }
     @LuaMadeCallable
-    public Integer incz(Integer z) {
-        return zz += z;
+    public LuaVec3i incz(Integer z) {
+        zz += z;
+        return this;
     }
 
     @LuaMadeCallable
-    public LuaVec3 inc(LuaVec3 vec) {
+    public LuaVec3i inc(LuaVec3i vec) {
         xx += vec.xx;
         yy += vec.yy;
         zz += vec.zz;
@@ -59,7 +60,12 @@ public class LuaVec3 extends LuaMadeUserdata {
     }
 
     @LuaMadeCallable
-    public LuaVec3 sum(LuaVec3 vec) {
-        return new LuaVec3(this).inc(vec);
+    public LuaVec3i sum(LuaVec3i vec) {
+        return new LuaVec3i(this).inc(vec);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("LuaVec(%s, %s, %s)", xx, yy, zz);
     }
 }
