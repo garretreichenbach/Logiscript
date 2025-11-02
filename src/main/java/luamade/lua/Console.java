3f409a5285db07ddd430cb1854b8f99e2a26eb7a
Garret Reichenbach <garretreichenbach@gmail.com>
2025-11-01 19:46:15 -0700

    Update Console.java

--- src/main/java/luamade/lua/Console.java
+++ src/main/java/luamade/lua/Console.java
@@ -1,14 +1,10 @@
 package luamade.lua;
 
-import com.bulletphysics.linearmath.Transform;
 import luamade.lua.element.block.Block;
 import luamade.luawrap.LuaMadeCallable;
 import luamade.luawrap.LuaMadeUserdata;
 import luamade.system.module.ComputerModule;
 import org.luaj.vm2.Varargs;
-import org.schema.game.client.view.effects.RaisingIndication;
-import org.schema.game.client.view.gui.shiphud.HudIndicatorOverlay;
-import org.schema.game.common.data.ManagedSegmentController;
 import org.schema.game.common.data.SegmentPiece;
 
 import java.util.PriorityQueue;
@@ -16,17 +12,26 @@ import java.util.Queue;
 
 public class Console extends LuaMadeUserdata {
 
-	private int VERTICAL = 0;
-	private int HORIZONTAL = 1;
-
 	private final ComputerModule module;
 	private final Queue<Object[]> printQueue = new PriorityQueue<>();
+	private int VERTICAL = 0;
+	private int HORIZONTAL = 1;
 	private StringBuilder textContents = new StringBuilder();
 	private int[] cursorPos = {0, 0};
 
 	public Console(ComputerModule module) {
 		this.module = module;
 		startPrintThread();
+	}
+
+	public static void sendError(SegmentPiece segmentPiece, String text) {
+		try {
+			System.err.println(text);
+			//Todo
+//			LuaManager.getModule(segmentPiece).getData(segmentPiece).lastOutput += text;
+		} catch(Exception exception) {
+			exception.printStackTrace();
+		}
 	}
 
 	@LuaMadeCallable
@@ -41,57 +46,36 @@ public class Console extends LuaMadeUserdata {
 
 	@LuaMadeCallable
 	public void print(Varargs vargs) {
-		printQueue.add(new Object[] {new Double[] {1.0, 1.0, 1.0, 1.0}, vargs});
+		printQueue.add(new Object[]{new Double[]{1.0, 1.0, 1.0, 1.0}, vargs});
 	}
 
 	@LuaMadeCallable
 	public void display(Varargs vargs) {
-		printQueue.add(new Object[] {new Double[] {1.0, 1.0, 1.0, 1.0}, true, vargs});
+		printQueue.add(new Object[]{new Double[]{1.0, 1.0, 1.0, 1.0}, true, vargs});
 	}
 
 	@LuaMadeCallable
 	public void printColor(Double[] color, Varargs vargs) {
-		printQueue.add(new Object[] {color, vargs});
+		printQueue.add(new Object[]{color, vargs});
 	}
 
 	@LuaMadeCallable
 	public void displayColor(Double[] color, Varargs vargs) {
-		printQueue.add(new Object[] {color, true, vargs});
+		printQueue.add(new Object[]{color, true, vargs});
 	}
 
 	@LuaMadeCallable
 	public void printError(Varargs vargs) {
-		printQueue.add(new Object[] {new Double[] {1.0, 0.3, 0.3, 1.0}, vargs});
+		printQueue.add(new Object[]{new Double[]{1.0, 0.3, 0.3, 1.0}, vargs});
 	}
 
 	@LuaMadeCallable
 	public void displayError(Varargs vargs) {
-		printQueue.add(new Object[] {new Double[] {1.0, 0.3, 0.3, 1.0}, true, vargs});
+		printQueue.add(new Object[]{new Double[]{1.0, 0.3, 0.3, 1.0}, true, vargs});
 	}
 
 	public SegmentPiece getSegmentPiece() {
 		return module.getSegmentPiece();
-	}
-
-	public void sendOutput(String string) {
-		try {
-			if(getSegmentPiece().getSegmentController() instanceof ManagedSegmentController) {
-				ManagedSegmentController<?> managedSegmentController = (ManagedSegmentController<?>) getSegmentPiece().getSegmentController();
-				//Todo: output to computer log or something
-			}
-		} catch(Exception exception) {
-			exception.printStackTrace();
-		}
-	}
-
-	public static void sendError(SegmentPiece segmentPiece, String text) {
-		try {
-			System.err.println(text);
-			//Todo
-//			LuaManager.getModule(segmentPiece).getData(segmentPiece).lastOutput += text;
-		} catch(Exception exception) {
-			exception.printStackTrace();
-		}
 	}
 
 	private void startPrintThread() {
@@ -107,20 +91,10 @@ public class Console extends LuaMadeUserdata {
 							if(objects[1] instanceof Boolean) display = (Boolean) objects[1];
 							Varargs vargs = (Varargs) objects[1];
 							StringBuilder string = new StringBuilder();
-							for(int i = 1; i <= vargs.narg() && i <= 16; ++i) string.append(vargs.arg(i).toString()).append("\n");
-							
-							// Append to textContents for GUI display
-							textContents.append(string);
-							
-							sendOutput(string.toString());
-							if(display) {
-								Transform transform = new Transform();
-								getSegmentPiece().getTransform(transform);
-								RaisingIndication raisingIndication = new RaisingIndication(transform, string.toString(), color[0].floatValue(), color[1].floatValue(), color[2].floatValue(), color[3].floatValue());
-								raisingIndication.speed = 0.1f;
-								raisingIndication.lifetime = 15.0f;
-								HudIndicatorOverlay.toDrawTexts.add(raisingIndication);
+							for(int i = 1; i <= vargs.narg() && i <= 16; ++i) {
+								string.append(vargs.arg(i).toString()).append("\n");
 							}
+							textContents.append(string);
 						}
 						sleep(2000);
 					}
@@ -167,7 +141,8 @@ public class Console extends LuaMadeUserdata {
 		if(cursorPos[VERTICAL] >= lines.length) return;
 		cursorPos[HORIZONTAL] = linePos;
 		trimCursorPos();
-		if(cursorPos[HORIZONTAL] > lines[cursorPos[VERTICAL]].length()) cursorPos[HORIZONTAL] = lines[cursorPos[VERTICAL]].length();
+		if(cursorPos[HORIZONTAL] > lines[cursorPos[VERTICAL]].length())
+			cursorPos[HORIZONTAL] = lines[cursorPos[VERTICAL]].length();
 		trimCursorPos();
 	}
 
